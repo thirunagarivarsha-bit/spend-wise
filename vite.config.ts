d41b6a4ece7fa0225510ea25f053bb069d6721a4
thirunagarivarsha-bit <thirunagari.varsha@klh.edu.in>
2026-06-17 15:30:30 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/spend-wise/',
+  base: process.env.GITHUB_ACTIONS ? '/spend-wise/' : '/',
 })
